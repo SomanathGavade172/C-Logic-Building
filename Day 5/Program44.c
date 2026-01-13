7694fe8546c53b55d381f105f54e89a8ff985373
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-01-13 23:25:52 +0530

    Day 3: Practiced for-loop statements

---
+++ Day 5/Program44.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name       :   CheckDivisible 
+//  Description         :   Checks whether the given number is divisible by 3 or not.
+//  Input               :   Integer
+//  Output              :   Boolean (true if divisible by 3, otherwise false)
+//  Author              :   Gavade Somanath Vilas
+//  Date                :   13/01/2026
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Definition
+bool CheckDivisible(int iNo)
+{
+    int iResult = 0;
+
+    iResult = iNo % 3;
+
+    if(iResult == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+    
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name    :   Check Divisibility.
+//  Description         :   This application accepts a number from the user and checks whether it is divisible by 3.
+//  
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter a Number : \n");
+    scanf("%d", &iValue);
+
+    bRet = CheckDivisible(iValue);
+
+    if(bRet == true)
+    {
+        printf("%d is divisible by 3\n", iValue);
+    }
+    else
+    {
+        printf("%d is not divisible by 3\n", iValue);
+    }
+
+    return 0;
+}

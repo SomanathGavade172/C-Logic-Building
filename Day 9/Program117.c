5d1b9d2eb3b85f4fa1db88ec060f7865b54cb58b
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-01-30 19:51:37 +0530

    Day 9: Added C Pattern Printing Programs

---
+++ Day 9/Program117.c
@@ -0,0 +1,61 @@
+/*
+    Input  :    Row = 4
+                Col = 3
+
+    Output :  
+                *   *   *
+                *   *   *
+                *   *   *
+                *   *   *
+                
+*/
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name       :   Display
+//  Description         :   Displays a rectangular star pattern using rows and columns.
+//  Input               :   Integer, Integer (Number of rows and columns)
+//  Output              :   NONE (Displays star pattern on the screen)
+//  Author              :   Gavade Somanath Vilas
+//  Date                :   30/01/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Definition
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)          // Outer loop controls rows
+    {
+        for(j = 1; j <= iCol; j++)     // Inner loop controls columns
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name    :   Rectangular Star Pattern Display Using Function
+//  Description         :   This application accepts number of rows and columns from the user and displays a rectangular star pattern.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of Columns : \n");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);        // Function Call.
+
+    return 0;
+}

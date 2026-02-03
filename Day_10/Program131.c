6aace7b0c994247cdd60419078271697fa5fb567
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-02-03 13:41:23 +0530

    Day 10: Advanced Alphabet Patterns in C

---
+++ Day_10/Program131.c
@@ -0,0 +1,60 @@
+/*
+    Input  : Row = 4  Col = 4
+     
+    Output :
+            a   a   a   a
+            b   b   b   b
+            c   c   c   c
+            d   d   d   d
+*/
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name  : Display
+//  Description    : Displays a pattern of lowercase alphabets row-wise, where each row contains the same alphabet.
+//  Input          : Integer (Rows), Integer (Columns)
+//  Output         : NONE (Displays pattern on console)
+//  Author         : Gavade Somanath Vilas
+//  Date           : 03/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Definition
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+    char Ch = 'a';
+
+    for(i = 1; i <= iRow; i++, Ch++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("%c\t", Ch);
+        }
+        printf("\n");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Alphabet Pattern Display
+//  Description      : This application accepts number of rows and columns and displays a character pattern using lowercase alphabets.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of Columns : \n");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);     // Function Call
+
+    return 0;
+}

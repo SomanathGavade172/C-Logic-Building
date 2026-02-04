dbb08449b2ea8def85fd86911d6e6535b63bfccd
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-02-04 22:33:49 +0530

    Day 10: Advanced Alphabet Patterns in C

---
+++ Day_10/Program138.c
@@ -0,0 +1,66 @@
+/*
+    Input  : Row = 4  Col = 4
+     
+    Output :
+                *
+                *   *
+                *   *   *
+                *   *   *   *
+*/
+
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name  : Display
+//  Description    : Displays a left-aligned right-angled triangle pattern using '*' symbols.
+//  Input          : Integer (Rows), Integer (Columns)
+//  Output         : NONE (Displays pattern on console)
+//  Author         : Gavade Somanath Vilas
+//  Date           : 03/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Definition
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    // Filter: pattern is valid only for square matrix
+    if(iRow != iCol)
+    {
+        printf("Invalid Input\n");
+        return;
+    }
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("*\t");
+        }
+        printf("\n");
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Left Triangle Star Pattern Display
+//  Description      : This application accepts number of rows and columns and displays a left-aligned triangular star pattern.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter number of Rows : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter number of Columns : \n");
+    scanf("%d", &iValue2);
+
+    Display(iValue1, iValue2);     // Function Call
+
+    return 0;
+}

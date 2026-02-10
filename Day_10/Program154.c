fefb50b10c00653a669f25ca78853914e8b0d1a5
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-02-10 23:24:21 +0530

    Day 10: Advanced Alphabet Patterns in C

---
+++ Day_10/Program154.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name    : CountSmall
+//  Description      : This function accepts a character pointer and counts the number of lowercase letters
+//                     (small characters) present in the given string.
+//                     It checks characters between ASCII values 97 ('a') and 122 ('z').
+//  Input            : Character Pointer (String)
+//  Output           : Integer (Returns count of lowercase letters)
+//  Author           : Gavade Somanath Vilas
+//  Date             : 10/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int CountSmall(char *ptr)
+{
+    int iCount = 0;
+
+    while(*ptr != '\0')
+    {
+        if(*ptr >= 97 && *ptr <= 122)
+        {
+            iCount++;
+        }        
+        ptr++;
+    }
+
+    return iCount;
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : Count of Small Letters Using Pointer
+//  Description      : This application accepts a string from the user and counts the number of lowercase letters present in the string using a function 
+//                     implemented with pointer traversal.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    char Arr[30];
+    int iRet = 0;
+
+    printf("Enter your Name : \n");
+    scanf("%[^\n]s", Arr);       // Reads input until Enter is pressed
+
+    iRet = CountSmall(Arr);
+
+    printf("count of small letters is : %d\n", iRet);
+ 
+    return 0;
+}

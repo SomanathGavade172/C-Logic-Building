cf5b58ecbf6c29f1ffc88a7b5123d6d6df63373f
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-02-10 23:22:31 +0530

    Day 10: Advanced Alphabet Patterns in C

---
+++ Day_10/Program150.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : String Input and Character Display Using Pointer
+//  Description      : This program accepts a name from the user using a character array and displays individual characters using a pointer.
+//                     It demonstrates how to take string input using scanf scanset to read input until newline.
+//  Input            : String (User Name)
+//  Output           : NONE (Displays characters of the entered name one by one)
+//  Author           : Gavade Somanath Vilas
+//  Date             : 10/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    char Arr[30];
+    char *ptr = Arr;
+
+    printf("Enter your Name : \n");
+    scanf("%[^'\n']s", Arr);       // Reads input until Enter is pressed
+
+    while(*ptr != '\0')
+    {
+        printf("%c\n", *ptr);
+        ptr++;
+    }
+ 
+    return 0;
+}

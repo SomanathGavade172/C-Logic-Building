d89ec188c553851991aed71dd68f918d8904f655
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-02-21 23:00:37 +0530

    Refactor Count function to check character presence
    Updated Count function to check for character presence instead of counting occurrences. Modified application to reflect the new functionality.

--- Day_11/Program176.c
+++ Day_11/Program176.c
@@ -1,58 +1,69 @@
 #include<stdio.h>
 
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //  Function Name    : Count
-//  Description      : This function accepts a character pointer and one character. It counts the number of occurrences of the given character in the string.
-//                     It traverses the string using pointer increment until it reaches the null terminator ('\0').
+//  Description      : This function accepts a character pointer (string) and a character. It checks whether the given character is present in the string.
+//                     The function traverses the string using pointer increment until it reaches the null terminator ('\0').
 //  Input            : Character Pointer (String), Character
-//  Output           : Integer (Returns count of given character)
+//  Output           : Boolean (Returns TRUE if character is found, otherwise FALSE)
 //  Author           : Gavade Somanath Vilas
-//  Date             : 20/02/2026
+//  Date             : 21/02/2026
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Function Definition
-int Count(char *str, char Ch)
+BOOL Count(char *str, char Ch)
 {
-    int iCount = 0;
+    BOOL bFlag = FALSE;
 
     while(*str != '\0')
     {
         if(*str == Ch)
         {
-            iCount++;
+            bFlag = TRUE;
+            break;
         }
-
         str++;
     }
-
-    return iCount;
+    return bFlag;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//  Application Name : Character Occurrence Count
-//  Description      : This application accepts a string from the user and counts the number of times a specific character
-//                     occurs in the string using a user-defined function implemented with pointer traversal.
+//  Application Name : Character Presence Checker
+//  Description      : This application accepts a string from the user and checks whether a specific character is present in the string using a user-defined function 
+//                     implemented with pointer traversal.
 //
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
     char Arr[30];
-    int iRet = 0;
+    BOOL bRet = FALSE;
     char cValue = '\0';
 
     printf("Enter a String : \n");
-    scanf("%[^'\n']s", Arr);
+    scanf("%[^'\n']s",Arr);
 
     printf("Enter a character : \n");
-    scanf(" %c", &cValue);   // Space added before %c
+    scanf(" %c", &cValue);   // Space added before %c to ignore previous newline
 
-    iRet = Count(Arr, cValue);           // Function Call.
+    bRet = Count(Arr, cValue);           // Function Call
 
-    printf("Number of '%c' is : %d\n", cValue, iRet);
+    if(bRet == TRUE)
+    {
+        printf("Character is present\n");
+    }
+    else
+    {
+        printf("Character is not present\n");
+    }
 
     return 0;
 }

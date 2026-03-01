1b2369c004f51483ebedb289a10eb0855cf117c0
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-03-01 23:05:56 +0530

    Day11: Added string input/output program

---
+++ Day_11/Program181.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name    : strtoggleX
+//  Description      : This function accepts a character pointer (string). It converts uppercase characters into lowercase and lowercase characters into uppercase.
+//                     The function traverses the string using pointer increment until it reaches the null terminator ('\0').
+//  Input            : Character Pointer (String)
+//  Output           : Void (String gets modified directly)
+//  Author           : Gavade Somanath Vilas
+//  Date             : 21/02/2026
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Definition
+void strtoggleX(char *str)
+{
+    while(*str != '\0')
+    {
+        if(*str >= 'a' && *str <= 'z')
+        {
+            *str = *str - 32;
+        }
+        else if(*str >= 'A' && *str <= 'Z')
+        {
+            *str = *str + 32;
+        }
+        str++;
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name : String Toggle Case Application
+//  Description      : This application accepts a string from the user and toggles the case of each alphabet character
+//                     using a user-defined function implemented with pointer traversal.
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    char Arr[30];
+
+    printf("Enter a String : \n");
+    scanf("%[^'\n']s", Arr);
+
+    strtoggleX(Arr);           // Function Call
+
+    printf("String after Updation is : %s\n", Arr);
+
+    return 0;
+}

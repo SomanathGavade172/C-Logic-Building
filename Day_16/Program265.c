ad68b1d7ed1ec6acbfb9880cfa1caf00c2db597e
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-09-12 22:09:59 +0530

    Day 15 : Added C Advanced Programs

---
+++ Day_16/Program265.c
@@ -0,0 +1,37 @@
+// *    *   *   *  
+
+#include<stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//  
+//  Function Name   :   DisplayTimeTable
+//  Description     :   Displays the exam time based on the student’s standard.
+//  Input           :   Int
+//  Output          :   None
+//  Auther          :   Gavade Somanath Vilas
+//  Date            :   09/05/2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Function Defination
+void Display()
+{
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application Name    :   Area of Circle
+//  Description         :   This application is used to calculate the area of a circle.
+//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    Display();      // Function Call.
+
+    return 0;
+}
